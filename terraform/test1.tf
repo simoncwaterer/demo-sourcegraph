d2b095f84766faff2cb8d144496a126926f6f7c6
Simon Waterer <waterer.simon@gmail.com>
2022-06-27 17:50:05 +0100

    Create test1.tf

---
+++ terraform/test1.tf
@@ -0,0 +1,10 @@
+locals {
+  name = "backstage"
+  
+  default_tags = {
+    Environment = var.environment
+    ServiceCatalogueId = 1
+    ServiceName = "myService"
+    RepositoryName = "myrepo"
+  }
+}
